0e9843ee778241425e4985034c51ffe5b057d271
Marcos Minond <minond.marcos@gmail.com>
2017-05-31 16:43:22 -0600

    moving grammar string into separate file

--- prompt.c
+++ prompt.c
@@ -4,22 +4,25 @@
 #include "readline.h"
 #include "vendor/mpc/mpc.h"
 
-const char * PROMPT = "lithp> ";
-const char * VERSION = "0.0.0";
+const char* PROMPT = "lithp> ";
+const char* VERSION = "0.0.0";
 
-int main(int argc, char ** argv) {
-  mpc_parser_t * Number = mpc_new("number");
-  mpc_parser_t * Operator = mpc_new("operator");
-  mpc_parser_t * Expr = mpc_new("expr");
-  mpc_parser_t * Lithp = mpc_new("lithp");
+char* read(char*);
 
-  mpca_lang(MPCA_LANG_DEFAULT,
-    "                                                         \
-    number    : /-?[0-9]+/;                                   \
-    operator  : \"add\" | '+' | '-' | '*' | '/' ;             \
-    expr      : <number > | '(' <operator> <expr>+ ')' ;      \
-    lithp     : /^/ <operator> <expr>+ /$/;                   \
-    ",
+int main(int argc, char** argv) {
+  char* grammar = read("grammar.txt");
+
+  if (!grammar) {
+    printf("failed to read grammar file");
+    exit(EXIT_FAILURE);
+  }
+
+  mpc_parser_t* Number = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr = mpc_new("expr");
+  mpc_parser_t* Lithp = mpc_new("lithp");
+
+  mpca_lang(MPCA_LANG_DEFAULT, grammar,
     Number, Operator, Expr, Lithp);
 
   printf("Lithp Version %s\n", VERSION);
@@ -27,7 +30,7 @@ int main(int argc, char ** argv) {
 
   while (1) {
     mpc_result_t result;
-    char * input = readline(PROMPT);
+    char* input = readline(PROMPT);
 
     if (mpc_parse("<stdin>", input, Lithp, &result)) {
       mpc_ast_print(result.output);
@@ -44,4 +47,26 @@ int main(int argc, char ** argv) {
   mpc_cleanup(4, Number, Operator, Expr, Lithp);
 
   return 0;
+}
+
+char* read(char* filename) {
+  char* buffer = 0;
+  long length;
+
+  FILE* file = fopen(filename, "r");
+
+  if (file) {
+    fseek(file, 0, SEEK_END);
+    length = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    buffer = malloc(length);
+
+    if (buffer) {
+      fread(buffer, 1, length, file);
+    }
+
+    fclose(file);
+  }
+
+  return buffer;
 }
